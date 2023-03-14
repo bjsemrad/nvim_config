82f4dd6ccbb5a9e66fa71a7946903eda08dbe08e
Brian Semrad <bjsemrad@gmail.com>
2023-03-13 21:08:57 -0500

    initial

---
+++ lua/cerebralvortex/init.lua
@@ -0,0 +1,6 @@
+require("cerebralvortex.remap")
+require("cerebralvortex.plugins")
+require("cerebralvortex.telescope")
+require("cerebralvortex.undo")
+require("cerebralvortex.fugitive")
+vim.cmd[[colorscheme dracula]]
